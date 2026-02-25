c891cd3a699227a430588fc71c7f646648315805
szymo <szymonsiwak1@gmail.com>
2026-02-25 20:21:10 +0100

    add favorites product prop, add many-to-many relation

--- ArtStore.Domain/Entities/User.cs
+++ ArtStore.Domain/Entities/User.cs
@@ -7,6 +7,8 @@ namespace ArtStore.Domain.Entities
 		public string FirstName { get; set; } = string.Empty;
 		public string LastName { get; set; } = string.Empty;
 
+		public ICollection<Product> Favorites { get; set; } = new List<Product>();
+
 		//TODO: add properties as address, order history, etc.
 	}
 }
---
+++ ArtStore.Infrastructure/Migrations/20260225191941_AddUserFavorites.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArtStore.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserFavorites : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserFavorites",
+                columns: table => new
+                {
+                    FavoritesId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserFavorites", x => new { x.FavoritesId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_UserFavorites_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserFavorites_Products_FavoritesId",
+                        column: x => x.FavoritesId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserFavorites_UserId",
+                table: "UserFavorites",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserFavorites");
+        }
+    }
+}
