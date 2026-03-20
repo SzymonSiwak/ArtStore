4d91613b2a80e5d8544d5fd9d1efb7e18af105f7
szymo <szymonsiwak1@gmail.com>
2026-03-20 15:55:54 +0100

    add new entities Category and Collection, add new prop for Product

---
+++ ArtStore.Domain/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using ArtStore.Domain.Common;
+
+namespace ArtStore.Domain.Entities
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        // User-friendly URL
+        public string URL { get; set; } = string.Empty;
+
+        //Relation Category-Product 1:N
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
---
+++ ArtStore.Domain/Entities/Collection.cs
@@ -0,0 +1,15 @@
+﻿using ArtStore.Domain.Common;
+
+namespace ArtStore.Domain.Entities
+{
+    public class Collection : BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Slug { get; set; } = string.Empty;
+        public string BannerImageUrl { get; set; } = string.Empty; 
+
+        // Relation Collection-Product (N:N)
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
